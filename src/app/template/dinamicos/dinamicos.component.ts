d377ede14b154384a00e1c323b40e74aa79b1b19
Mario Hernandez Yañez <maheya@MacBook-Pro-de-Mario.local>
2022-11-26 06:24:40 -0600

    dinamicos terminados

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+
+  nombre:string;
+  favoritos:Favorito[];
+}
+
+
+interface Favorito{
+  id: number;
+  nombre:string;
+}
+
+
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+export class DinamicosComponent {
 
-  ngOnInit(): void {
+  nuevoJuego : string = '';
+
+  persona :Persona = {
+    nombre: 'Mario',
+    favoritos: [
+      {id:1,nombre:'Silent hill'}, 
+      {id:2,nombre:'LIMBO'},
+    ]
+  }
+
+  guardar(){
+
+    console.log('form posteado');
+  }
+
+  eliminar( index:number ){
+    this.persona.favoritos.splice(index,1);
+  }
+
+  agregar(){
+
+    const nuevoFavorito:Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push(nuevoFavorito);
+    this.nuevoJuego = '';
   }
 
 }
